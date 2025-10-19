4c91fa526d17fbe770d40354b4725c68cc289f5a
dragonbough <fiveteen81@gmail.com>
2025-10-19 02:46:10 +0100

    added database data manipulation and simplified DB
    - removed SubTodo from DB -- everything is now contained in the Todo
    table using a ParentID
    - DB can now retrieve todo data from DB, returning in dict format
    - DB can also update todo data in DB
    	- either UPDATES or INSERTS new data depending on if the todo	_data
    already exists in DB or not
    - other small changes
    
    :D data

--- data.py
+++ data.py
@@ -5,12 +5,39 @@ import sqlite3
 if not os.path.isfile("tomo_data.db"):
     connection = sqlite3.connect("tomo_data.db")
     cursor = connection.cursor()
-    cursor.executescript("""CREATE TABLE IF NOT EXISTS "SubTodo"(ParentTodo INTEGER, ChildTodo INTEGER, PRIMARY KEY(ParentTodo, ChildTodo), FOREIGN KEY(ParentTodo) REFERENCES "Todo"(ID),
-                         FOREIGN KEY(ChildTodo) REFERENCES "Todo"(ID));
-                         CREATE TABLE sqlite_sequence(name,seq);
-                         CREATE TABLE Todo (ID Integer PRIMARY KEY AUTOINCREMENT, Name VARCHAR NOT NULL, Difficulty INTEGER, Completed INTEGER CHECK (Completed == 1 OR Completed == 0),
-                         Points INTEGER, TimeCreated INTEGER NOT NULL, TimeCompleted INTEGER);""")
+    cursor.executescript("""CREATE TABLE Todo (ID Integer PRIMARY KEY AUTOINCREMENT, Name VARCHAR NOT NULL, Difficulty INTEGER, Completed INTEGER CHECK (Completed == 1 OR Completed == 0),
+ Points INTEGER, TimeCreated INTEGER NOT NULL, TimeCompleted INTEGER, ParentID INTEGER REFERENCES Todo (ID));""")
     connection.commit()
 else:
     connection = sqlite3.connect("tomo_data.db")
     cursor = connection.cursor()
+
+#rows are returned as indexed dictionary instead of tuples
+connection.row_factory = sqlite3.Row
+
+
+#retrieve todo data based on id, or if not given, retrieve all todos from DB
+def retrieve_todo_data(*todo_ids : int) -> list[dict]:
+    if todo_ids:
+        todo_data = cursor.fetchall("SELECT * FROM Todo WHERE ID in (?)", todo_ids)
+    else:
+        todo_data = cursor.fetchall("SELECT * FROM Todo")
+    return todo_data
+
+#updates todo data if its id already exists in db
+#otherwise creates new entries in db
+def update_todo_data(*todo_data : iter[dict]):
+
+    existing_todos = []
+    new_todos = []
+
+    for todo_datum in todo_data:
+        if todo_datum["id"]:
+            existing_todos.append(todo_datum)
+        else:
+            new_todos.append(todo_datum)
+
+    if existing_todos:
+        cursor.executemany("UPDATE Todo SET Name = (?), Difficulty = (?), Completed = (?), Points = (?), TimeCompleted = (?) WHERE ID = (?)", [(todo["name"], todo["difficulty"], todo["completed"], todo["points"], todo["timecompleted"], todo["id"]) for todo in existing_todos])
+    if new_todos:
+        cursor.executemany("INSERT INTO Todo (Name, Difficulty, Completed, Points, TimeCreated, TimeCompleted, ParentID) VALUES ((?), (?), (?), (?), (?), (?), (?))", [(todo["name"], todo["difficulty"], todo["completed"], todo["points"], todo["timecreated"], todo["timecompleted"], todo["parentid"]) for todo in new_todos])
