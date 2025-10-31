5e28a487d92dfd4fb839760af3c6bc5251f49c4f
dragonbough <fiveteen81@gmail.com>
2025-10-31 03:26:02 +0000

    fixed retrieving and deleting todos + other fixes
    - connection row factory actually works now -- data is now retrieved
    using column names
    - variable number of todos can now be deleted/retrieved -- number of
    bindings changes with number of todos
    - existing/new todos split based on if id is string or int -- "untitled
    xx" vs xx
    - other small changes

--- data.py
+++ data.py
@@ -1,38 +1,41 @@
 import os
 import sqlite3
 
+connection = sqlite3.connect("tomo_data.db")
+#rows are returned as indexed dictionary instead of tuples
+connection.row_factory = sqlite3.Row
+cursor = connection.cursor()
+
 #if db doesn't exist here, constructs db using schema commands
 if not os.path.isfile("tomo_data.db"):
-    connection = sqlite3.connect("tomo_data.db")
-    cursor = connection.cursor()
     cursor.executescript("""CREATE TABLE Todo (ID Integer PRIMARY KEY AUTOINCREMENT, Name VARCHAR NOT NULL, Difficulty INTEGER, Completed INTEGER CHECK (Completed == 1 OR Completed == 0),
  Points INTEGER, TimeCreated INTEGER NOT NULL, TimeCompleted INTEGER, ParentID INTEGER REFERENCES Todo (ID));""")
     connection.commit()
-else:
-    connection = sqlite3.connect("tomo_data.db")
-    cursor = connection.cursor()
-
-#rows are returned as indexed dictionary instead of tuples
-connection.row_factory = sqlite3.Row
 
 
 #retrieve todo data based on id, or if not given, retrieve all todos from DB
 def retrieve_todo_data(*todo_ids : int) -> list[dict]:
     if todo_ids:
-        todo_data = cursor.fetchall("SELECT * FROM Todo WHERE ID in (?)", todo_ids)
+        if len(todo_ids) == 1:
+            bindings = "?"
+        else:
+            bindings = "?, " * (len(todo_ids) -1) + "?"
+        cursor.execute(f"SELECT * FROM Todo WHERE ID in ({bindings})", todo_ids)
+        todo_data = cursor.fetchall()
     else:
-        todo_data = cursor.fetchall("SELECT * FROM Todo")
+        cursor.execute("SELECT * FROM Todo")
+        todo_data = cursor.fetchall()
     return todo_data
 
 #updates todo data if its id already exists in db
 #otherwise creates new entries in db
-def update_todo_data(*todo_data : list[dict]):
+def modify_todo_data(*todo_data : dict):
 
     existing_todos = []
     new_todos = []
 
     for todo_datum in todo_data:
-        if todo_datum["id"]:
+        if type(todo_datum["id"]) != str:
             existing_todos.append(todo_datum)
         else:
             new_todos.append(todo_datum)
@@ -41,3 +44,18 @@ def update_todo_data(*todo_data : list[dict]):
         cursor.executemany("UPDATE Todo SET Name = (?), Difficulty = (?), Completed = (?), Points = (?), TimeCompleted = (?) WHERE ID = (?)", [(todo["name"], todo["difficulty"], todo["completed"], todo["points"], todo["timecompleted"], todo["id"]) for todo in existing_todos])
     if new_todos:
         cursor.executemany("INSERT INTO Todo (Name, Difficulty, Completed, Points, TimeCreated, TimeCompleted, ParentID) VALUES ((?), (?), (?), (?), (?), (?), (?))", [(todo["name"], todo["difficulty"], todo["completed"], todo["points"], todo["timecreated"], todo["timecompleted"], todo["parentid"]) for todo in new_todos])
+
+    connection.commit()
+
+#deletes todos from db based on id
+def delete_todo_data(*todo_ids : int):
+
+    if todo_ids:
+        if len(todo_ids) == 1:
+            bindings = "?"
+        else:
+            bindings = "?, " * (len(todo_ids) -1) + "?"
+        cursor.execute(f"DELETE FROM Todo WHERE ID in ({bindings})", todo_ids)
+        connection.commit()
+
+
