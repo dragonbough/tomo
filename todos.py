e48bb1b97a94c9991390a4d8cc85272962b079f5
dragonbough <fiveteen81@gmail.com>
2025-11-05 12:06:44 +0000

    changed handling of "local" todos (yet to be saved to the DB) + many other smaller changes
    - Todos are auto-adopted on creation depending on the "parent" parameter
    - new "local" attribute used to describe todos not yet saved in the DB
    	- local todos now assume id as the latest id in the to-do list
    - todos being adopted now are "disowned" by previous parent
    - todos now actually retrieve completion parameters
    - get_todos() method created to retrieve all todos
    - todos binned to the delete bin are now recursively deleted (in order
    to delete their children as well)
    - todos will not be added to a bin again if already present in that bin
    - todos that are being deleted will be removed from the modify bin as
    they no longer need to be saved
    - quality of life + feature improvements made to the CLI tool
    - other small changes

--- todos.py
+++ todos.py
@@ -1,5 +1,5 @@
 import data
-import datetime
+from datetime import datetime
 import sys
 import os
 from typing import Literal
@@ -12,17 +12,28 @@ class Todo():
         self.difficulty = difficulty
         self.parent : Todo
         self.parent = parent
+        if self.parent:
+            self.parent.adopt(self)
         self.completed = completed
         self.children = []
         self.points = points
-        self.time_created = datetime.datetime.now().timestamp()
+        self.time_created = datetime.now().timestamp()
         self.time_completed = time_completed
         self.deleted = False
+        self.local = False
 
+    #the todo's previous parent disowns it and this new parent adopts it
     def adopt(self, todo : "Todo"):
+        if todo.parent:
+            todo.parent.disown(todo)
         self.children.append(todo)
         todo.parent = self
 
+    #todo gets rid of parent and parent gets rid of todo
+    def disown(self, todo : "Todo"):
+        if todo in self.children:
+            self.children.remove(todo)
+        todo.parent = None
 
 class TodoList():
 
@@ -32,16 +43,18 @@ class TodoList():
         todos = []
         foster_dict = {}
 
-        for todo_data in data.retrieve_todo_data():
-            todo_id = int(todo_data["id"])
-            name = todo_data["name"]
-            difficulty = int(todo_data["difficulty"])
+        for todo_datum in data.retrieve_todo_data():
+            todo_id = int(todo_datum["id"])
+            name = todo_datum["name"]
+            difficulty = int(todo_datum["difficulty"])
+            completed = True if todo_datum["completed"] else False
+            time_completed = todo_datum["timecompleted"]
 
-            todo = Todo(name, todo_id, difficulty)
+            todo = Todo(name=name, todo_id=todo_id, difficulty=difficulty, completed=completed, time_completed=time_completed)
 
             #adds itself under parent id
             #so it can be adopted later
-            parent_id = todo_data["parentid"]
+            parent_id = todo_datum["parentid"]
             if parent_id in foster_dict:
                 foster_dict[parent_id].append(todo)
             else:
@@ -64,32 +77,30 @@ class TodoList():
         self.add_todos(*todos)
         self.bin = {"modify" : [], "delete" : []}
 
+
     #retrieves a todo from the todo list based on either todo_id or todo_name -- better way than accessing attributes directly
     def get_todo(self, *todo_ids : int | str) -> Todo:
         todos = []
 
         if todo_ids:
+
             for todo_id in todo_ids:
+
                 if todo_id in self.todos:
                     todos.append(self.todos[todo_id])
                 else:
                     raise NameError(f"To-do ID {todo_id} not in To-do list")
 
-        # elif todo_names:
-        #     todo_name_dict = {todo.name : todo for todo in self.todos.values()}
-        #     for todo_name in todo_names:
-        #         if todo_name in todo_name_dict:
-        #             todos.append(todo_name_dict[todo_name])
-        #         else:
-        #             raise NameError(f"To-do named {todo_name} not in To-do list")
-
         return todos[0] if len(todos) == 1 else todos
 
+    def get_todos(self) -> list[Todo]:
+        return list(self.todos.values())
+
     def get_roots(self):
-        return [todo for todo in self.todos.values() if not todo.parent]
+        return [todo for todo in self.get_todos() if not todo.parent]
 
     #initialises dfs algo with root nodes and retrieves sorted list of todos -- sorts each tree and appends to each other
-    #if returning depth information is enabled, a dict of visited nodes and the todo ids to their depths will be sent
+    #if returning depth information is enabled, a dict of visited nodes and the todo depths will be sent
     def sort_todos(self, return_depths = False) -> list[Todo]:
         root_nodes = self.get_roots()
         sorted_todos = {"visited" : [], "depths" : {}} if return_depths else []
@@ -118,7 +129,6 @@ class TodoList():
             else:
                 node_depths[node.todo_id] = depth
 
-
         visited.append(node)
 
         if return_depths and node.children:
@@ -142,26 +152,32 @@ class TodoList():
             raise NameError("Invalid bin type.")
         if bin_type == "delete":
             todo.deleted = True
-        self.bin[bin_type].append(todo.todo_id)
+            for child in todo.children:
+                self.bin_todo(child, "delete")
+
+        if todo.todo_id not in self.bin[bin_type]:
+            self.bin[bin_type].append(todo.todo_id)
 
     #creates a todo, adding it to todolist and binning it for modifying
     def create_todo(self, name : str, parent : Todo, difficulty : int = 0):
         todo = Todo(name=name, difficulty=difficulty, parent=parent)
         #give the todo a temporary id before replacing when saved to DB
-        todo.todo_id = "untitled " + str(max([todo_id for todo_id in self.todos if type(todo_id) == int]) + 1)
+        #setting the local attribute to True to make sure DB knows it doesnt exist in DB yet
+        todo.todo_id = max(self.todos) + 1 if self.todos else 1
+        todo.local = True
         self.add_todos(todo)
         self.bin_todo(todo, "modify")
 
     #completes todo recursively
-    def complete_todo(self, todo : Todo, completion : bool):
+    def complete_todo(self, todo : Todo, completion : bool = None):
+        if completion == None:
+            completion = not todo.completed
         todo.completed = completion
         for child in todo.children:
-            self.complete_todo(child)
+            self.complete_todo(child, completion)
 
     #formats all Todo data into a dictionary for DB
     def unpack_todos(self, *todos : Todo):
-        if not todos:
-            todos = list(self.todos.values())
         todo_data = []
         for todo in todos:
             todo : Todo
@@ -174,7 +190,8 @@ class TodoList():
             todo_datum["timecreated"] = todo.time_created
             todo_datum["timecompleted"] = todo.time_completed
             todo_datum["parentid"] = todo.parent.todo_id if todo.parent else None
-            #how to add parentid if it doesn't exist yet
+            todo_datum["local"] = todo.local
+
             todo_data.append(todo_datum)
 
         return todo_data
@@ -188,6 +205,11 @@ class TodoList():
 
     #deletes the todos in the delete bin
     def empty_deleted(self):
+
+        #removes them from the modify bin, to prevent them from being saved to later
+        for todo_id in self.bin["delete"]:
+            if todo_id in self.bin["modify"]:
+                self.bin["modify"].remove(todo_id)
 
         data.delete_todo_data(*self.bin["delete"])
 
@@ -203,11 +225,22 @@ if __name__ == "__main__":
     def display_todos(todo_list : TodoList, selected = None):
 
         start_bold = "\033[1m"
-        end_bold = "\033[0m"
+        start_green = "\033[92m"
+        end_format = "\033[0m"
+
         sorted_todos = todo_list.sort_todos(return_depths=True)
+
+        points = ["•", "◦", "▪"]
+
         for todo in sorted_todos["visited"]:
-            todo_id = todo.todo_id if type(todo.todo_id) != str else todo.todo_id.replace("untitled ", "")
-            print(f"""{start_bold if selected == todo else ""}[{todo_id}] {" "*sorted_todos["depths"][todo.todo_id]}{todo.name} [{"X" if todo.completed else ""}] {end_bold if selected == todo else ""}""")
+
+            indent = sorted_todos["depths"][todo.todo_id]
+            point = points[indent % len(points)]
+
+            padding = " " * ( len(str(max(todo_list.todos))) - len(str(todo.todo_id)) )
+
+            print(f"""{start_bold if selected == todo else ""}[{todo.todo_id}] {start_green if todo.completed else ""}{padding}{"   " * indent}{point} {todo.name} {"X" if todo.completed else ""} {end_format if selected == todo else ""}{end_format if todo.completed else ""}""")
+
 
     def clear_screen():
         # For Windows
@@ -217,6 +250,7 @@ if __name__ == "__main__":
         else:
             _ = os.system('clear')
 
+
     user_todo_list = TodoList.get_user_todos()
     running = True
     hud = False
@@ -225,36 +259,98 @@ if __name__ == "__main__":
 
     while running:
 
-        # clear_screen()
+        clear_screen()
         display_todos(user_todo_list, selected_todo)
 
         if not selected_todo:
-            user_input = input("\n[X] Exit\n")
-            if user_input.lower() == "x":
-                user_todo_list.empty_bin()
-                sys.exit()
+            user_input = input("\n[A] Create To-do  [X] Exit\n").lower()
+            if user_input == "a":
+                todo_name = input("To-do Name: ")
+                todo_difficulty = int(input("To-do Difficulty: "))
+                user_todo_list.create_todo(name=todo_name, parent=None, difficulty=todo_difficulty)
 
         if selected_todo:
             clear_screen()
             display_todos(user_todo_list, selected_todo)
-            user_input = input(f"\n[A] Edit  [S] Create Sub-Todo  [D] Delete\n")
+            user_input = input(f"\n[M] Mark Completed  [A] Create Sub-Todo  [S] Edit  [D] Delete  [F] View  [X] Exit\n")
 
             if not user_input.isdecimal():
 
                 user_input = user_input.lower()
 
-                if user_input == "a":
-                    user_input = input([parameter for parameter in user_todo_list.unpack_todos().keys()])
+                if user_input == "m":
+                    user_todo_list.complete_todo(selected_todo)
+                    user_todo_list.bin_todo(selected_todo, "modify")
 
-                elif user_input == "s":
+                elif user_input == "a":
                     todo_name = input("To-do Name: ")
-                    todo_difficulty = int(input("To-do Difficulty: "))
+                    todo_difficulty = -1
+                    while not(1 <= todo_difficulty <= 4):
+                        todo_difficulty = int(input("To-do Difficulty: [1] Trivial  [2] Easy  [3] Hard  [4] Very Hard\n"))
                     user_todo_list.create_todo(name=todo_name, parent=selected_todo, difficulty=todo_difficulty)
 
-                elif user_input == "d":
-                    user_todo_list.bin_todo(selected_todo, "delete")
-                    selected_todo = None
 
+                if user_input == "s":
+                    edit_option = input(f"[A] Name  [S] Difficulty [D] Parent\n").lower()
+
+                    if edit_option == "a":
+                        selected_todo.name = input("\nEdit To-do Name: ")
+
+                    elif edit_option == "s":
+                        selected_todo.difficulty = int(input("\nEdit To-do Difficulty: "))
+
+                    elif edit_option == "d":
+                        clear_screen()
+                        display_todos(user_todo_list, selected_todo)
+                        parent_id = input("\nEnter ID of New Parent or [ENTER] to Disown: ")
+
+                        old_parent = None
+
+                        if not parent_id:
+
+                            if selected_todo.parent:
+                                old_parent = selected_todo.parent
+                                selected_todo.parent.disown(selected_todo)
+
+                        else:
+
+                            parent_id = int(parent_id)
+
+                            if parent_id in user_todo_list.todos and parent_id != selected_todo.todo_id:
+                                parent_todo = user_todo_list.get_todo(parent_id)
+                                parent_todo.adopt(selected_todo)
+
+                        user_todo_list.bin_todo(selected_todo, "modify")
+                        if old_parent:
+                            user_todo_list.bin_todo(old_parent, "modify")
+                        if selected_todo.parent:
+                            user_todo_list.bin_todo(selected_todo.parent, "modify")
+
+
+                elif user_input == "d":
+                    choice = input("Are you sure? [Y][N]\n").lower()
+                    if choice.lower() == "y":
+                        user_todo_list.bin_todo(selected_todo, "delete")
+                        selected_todo = None
+                    else:
+                        continue
+
+                elif user_input == "f":
+
+                    choice = None
+                    time_created = datetime.fromtimestamp(selected_todo.time_created).strftime("%Y/%m/%d, %H:%M:%S")
+                    time_completed = datetime.fromtimestamp(selected_todo.time_completed).strftime("%Y/%m/%d, %H:%M:%S") if selected_todo.time_completed else None
+
+                    while choice != "f":
+
+                        clear_screen()
+                        print(f"\033[1m{selected_todo.name}\033[0m")
+                        print(f"Time Created: {time_created}  Time Completed: {time_completed}  Difficulty: {selected_todo.difficulty}\n")
+                        choice = input("[F] Close\n").lower()
+
+        if user_input == "x":
+                user_todo_list.empty_bin()
+                sys.exit()
 
         if user_input.isdecimal():
                 if int(user_input) in user_todo_list.todos:
