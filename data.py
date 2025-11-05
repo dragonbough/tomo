4034ecd1f8af1cc0ed217d890d8677e4b5bb4571
dragonbough <fiveteen81@gmail.com>
2025-11-05 11:49:15 +0000

    improvements in saving todos + small changes
    - data now checks for if todo is labelled as "local" to differentiate
    between new and existing todos, instead of the previous "untitled"
    functionality
    - new system added that saves local todos with local parents + children
    hierarchically to maintain correct parent_id as data is saved, using a
    DFS algo: dfs_modify_todo_data()
    - ID auto increment is reset whenever the DB is empty

--- data.py
+++ data.py
@@ -13,6 +13,9 @@ if not os.path.isfile("tomo_data.db"):
     connection.commit()
 
 
+
+### TODO DATA ###
+
 #retrieve todo data based on id, or if not given, retrieve all todos from DB
 def retrieve_todo_data(*todo_ids : int) -> list[dict]:
     if todo_ids:
@@ -35,17 +38,59 @@ def modify_todo_data(*todo_data : dict):
     new_todos = []
 
     for todo_datum in todo_data:
-        if type(todo_datum["id"]) != str:
+        if todo_datum["local"] == False:
             existing_todos.append(todo_datum)
         else:
             new_todos.append(todo_datum)
 
     if existing_todos:
-        cursor.executemany("UPDATE Todo SET Name = (?), Difficulty = (?), Completed = (?), Points = (?), TimeCompleted = (?) WHERE ID = (?)", [(todo["name"], todo["difficulty"], todo["completed"], todo["points"], todo["timecompleted"], todo["id"]) for todo in existing_todos])
+        cursor.executemany("UPDATE Todo SET Name = (?), Difficulty = (?), Completed = (?), Points = (?), TimeCompleted = (?), ParentID = (?) WHERE ID = (?)", [(todo["name"], todo["difficulty"], todo["completed"], todo["points"], todo["timecompleted"], todo["parentid"], todo["id"]) for todo in existing_todos])
+
+    #save all of the new todos with no parents or children that are also new
+    #for the remaining todos (they have local parents or local children), use DFS to save in order, storing the last row_id before passing it on to the parentid of the children
     if new_todos:
-        cursor.executemany("INSERT INTO Todo (Name, Difficulty, Completed, Points, TimeCreated, TimeCompleted, ParentID) VALUES ((?), (?), (?), (?), (?), (?), (?))", [(todo["name"], todo["difficulty"], todo["completed"], todo["points"], todo["timecreated"], todo["timecompleted"], todo["parentid"]) for todo in new_todos])
+
+        search_set = []
+        fully_local_todo_data = []
+
+        new_todos_parent_ids = [todo_datum["parentid"] for todo_datum in new_todos]
+        new_todos_ids = [todo_datum["id"] for todo_datum in new_todos]
+
+        for todo_datum in new_todos:
+            if todo_datum["id"] not in new_todos_parent_ids and todo_datum["parentid"] not in new_todos_ids:
+                fully_local_todo_data.append(todo_datum)
+            else:
+                search_set.append(todo_datum)
+
+        cursor.executemany("INSERT INTO Todo (Name, Difficulty, Completed, Points, TimeCreated, TimeCompleted, ParentID) VALUES ((?), (?), (?), (?), (?), (?), (?))", [(todo["name"], todo["difficulty"], todo["completed"], todo["points"], todo["timecreated"], todo["timecompleted"], todo["parentid"]) for todo in fully_local_todo_data])
+
+        # print(f"Search set: {[datum["name"] for datum in search_set]}")
+
+        search_set_roots = [todo_datum for todo_datum in search_set if todo_datum["parentid"] not in new_todos_ids]
+
+        # print(f"Search set roots: {[datum["name"] for datum in search_set_roots]}")
+
+        todo_datum_heirarchy = {}
+        for todo_datum in search_set:
+            todo_datum_heirarchy[todo_datum["id"]] = [child_datum for child_datum in search_set if child_datum["parentid"] == todo_datum["id"]]
+
+        for root_datum in search_set_roots:
+            dfs_modify_todo_data(datum=root_datum, visited=[], new_parent_id=root_datum["parentid"], heirarchy=todo_datum_heirarchy)
 
     connection.commit()
+
+def dfs_modify_todo_data(datum : dict, visited : list, new_parent_id : int, heirarchy : dict):
+    datum["parentid"] = new_parent_id
+    visited.append(datum)
+    cursor.execute("INSERT INTO Todo (Name, Difficulty, Completed, Points, TimeCreated, TimeCompleted, ParentID) VALUES ((?), (?), (?), (?), (?), (?), (?))", (datum["name"], datum["difficulty"], datum["completed"], datum["points"], datum["timecreated"], datum["timecompleted"], datum["parentid"]))
+    new_parent_id = cursor.lastrowid
+
+    if heirarchy[datum["id"]]:
+        for child in heirarchy[datum["id"]]:
+            dfs_modify_todo_data(child, visited, new_parent_id, heirarchy)
+
+    if datum in visited:
+        return
 
 #deletes todos from db based on id
 def delete_todo_data(*todo_ids : int):
@@ -58,4 +103,16 @@ def delete_todo_data(*todo_ids : int):
         cursor.execute(f"DELETE FROM Todo WHERE ID in ({bindings})", todo_ids)
         connection.commit()
 
+        #if theres nothing else in the DB then use delete command to ensure the restart the auto increment of ids
+        #if you are having continuity issues with other modules in tomo, remove the reset of the auto increment
+        cursor.execute(f"SELECT * FROM Todo")
+        if not cursor.fetchall():
+            cursor.execute("UPDATE SQLITE_SEQUENCE SET SEQ = 0 WHERE NAME = 'Todo'")
 
+        connection.commit()
+
+
+
+### TOMO DATA ###
+
+#retrieve all tomo data
