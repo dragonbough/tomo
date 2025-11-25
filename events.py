bc8cb0fd6b45482656cdfe809e55514da99829ab
dragonbough <fiveteen81@gmail.com>
2025-11-25 07:32:01 +0000

    created observer + observable event system for interaction between tomos and todos
    - Observable() class acts as an event that observers can register their
    interest to or trigger
    	- registering interest just passes a callback
    	- triggering calls all of the callbacks passed into Observable, with
    optional args
    - Channel() class stores Observables to specific channels/contexts
    - created premade TOMO_COMPLETED event specific to the premade
    todo_channel
    	- todos.py triggers event when todo completed
    	- tomos.py registers function to this that increases tomo xp

---
+++ events.py
@@ -0,0 +1,40 @@
+import time
+
+class Observable():
+
+    def __init__(self, event_name : str):
+        self.name = event_name
+        # All of the functions that occur when this event is triggered
+        self.callbacks = []
+
+    def register(self, callback : callable):
+        self.callbacks.append(callback)
+
+    # triggers all of the registered functions
+    def trigger(self, *event_args):
+        callback_number = len(self.callbacks)
+        print(f"Executing {callback_number} callback{"s" if callback_number > 1 else ""}...")
+        for callback in self.callbacks:
+            callback(*event_args)
+        time.sleep(1)
+
+class Channel():
+
+    def __init__(self):
+        self.channel = {}
+        self.channel : dict[str : Observable]
+
+    def create_event(self, event_name : str):
+        event = Observable(event_name=event_name)
+        self.channel[event_name] = event
+
+    def get_event(self, event_name : str) -> Observable:
+        if event_name in self.channel:
+            return self.channel[event_name]
+
+# channel for events from todos.py
+todo_channel = Channel()
+todo_channel.create_event("TODO_COMPLETED")
+
+# channel for events from tomos.py
+tomo_channel = Channel()
