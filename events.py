f34afa4f3586712cff0402101c44b084bb578281
dragonbough <fiveteen81@gmail.com>
2025-12-22 11:35:56 +0000

    added event stream + renamed channel system + added more events
    - channels have now be renamed to "topics"
    	- this is more accurate to their function. they more so categorise the
    different types of events, rather than add new functionality
    - added event stream system
    	- events, when triggered, are now logged with a timestamp (how long the
    program has been running) in a tuple
    	- these logs are stored in a Stream object that is assigned to each
    topic
    	- the entirestream / individual logs can then be retrieved to see the
    timeline of certain events for certain features (e.g increasing tomo xp
    based on amount of pomodoro rounds completed)
    - added events for tomo topic for when xp is increased and level is
    increased
    - added events for pomodoro system for when timer is paused, and for the
    focus period feature

--- events.py
+++ events.py
@@ -1,11 +1,23 @@
 import time
 
+# start time used for timestamps
+program_start_time = time.time()
+
+# returns the current time in seconds since the program started
+def get_program_elapsed():
+    return program_start_time - time.time()
+
 class Observable():
 
     def __init__(self, event_name : str):
         self.name = event_name
         # All of the functions that occur when this event is triggered
         self.callbacks = []
+        self.topic = None
+        self.topic : "Topic"
+
+    def assign_topic(self, topic : "Topic"):
+        self.topic = topic
 
     def register(self, callback : callable):
         self.callbacks.append(callback)
@@ -14,33 +26,72 @@ class Observable():
     def trigger(self, *event_args):
         # callback_number = len(self.callbacks)
         # print(f"Executing {callback_number} callback{"s" if callback_number > 1 or callback_number == 0 else ""}...")
+        self.topic.log_event(event_name=self.name, timestamp=get_program_elapsed())
         for callback in self.callbacks:
             callback(*event_args)
         # time.sleep(1)
 
-class Channel():
+class Topic():
 
     def __init__(self):
-        self.channel = {}
-        self.channel : dict[str : Observable]
+        self.events = {}
+        self.events : dict[str : Observable]
+        # stores record of events since program execution
+        self.stream = Stream()
 
+    # creates an event undet this Topic object and assigns it to self
     def create_event(self, event_name : str):
         event = Observable(event_name=event_name)
-        self.channel[event_name] = event
+        self.events[event_name] = event
+        event.assign_topic(self)
 
+    # returns a specific event based on its name
     def get_event(self, event_name : str) -> Observable:
-        if event_name in self.channel:
-            return self.channel[event_name]
+        if event_name in self.events:
+            return self.events[event_name]
 
-# channel for events from todos.py
-todo_channel = Channel()
-todo_channel.create_event("TODO_COMPLETED")
+    # logs event by adding to its stream
+    def log_event(self, event_name : str, timestamp : float):
+        self.stream.log_event(event_name = event_name, timestamp=timestamp)
 
-# channel for events from tomos.py
-tomo_channel = Channel()
 
-# channel for events from pomos.py
-pomo_channel = Channel()
-pomo_channel.create_event("TIMER_COMPLETED")
-pomo_channel.create_event("TIMER_ITERATED")
-pomo_channel.create_event("ROUND_COMPLETED")
+# logs the occurence of events since program started
+class Stream():
+
+    def __init__(self):
+        # stream acts as queue kinda, storing tuple of timestamp and event name
+        self.stream = []
+        self.stream : list[tuple[float, str]]
+
+
+    # logs the occurence of an event to its stream, using timestamp and event in tuple
+    def log_event(self, event_name : str, timestamp : float):
+        self.stream.append((timestamp, event_name))
+
+    # returns the entire stream of events (from latest to last)
+    def get_stream(self):
+        return self.stream[::-1]
+
+    # get last timstamp of event
+    def get_last(self, event_name : str):
+        for event_log in self.get_stream():
+            if event_log[1] == event_name:
+                return event_log
+
+
+# topic for events from todos.py
+todo_topic = Topic()
+todo_topic.create_event("TODO_COMPLETED")
+
+# topic for events from tomos.py
+tomo_topic = Topic()
+tomo_topic.create_event("XP_INCREASED")
+tomo_topic.create_event("LVL_INCREASED")
+
+# topic for events from pomos.py
+pomo_topic = Topic()
+pomo_topic.create_event("TIMER_COMPLETED")
+pomo_topic.create_event("TIMER_ITERATED")
+pomo_topic.create_event("ROUND_COMPLETED")
+pomo_topic.create_event("TIMER_PAUSED")
+pomo_topic.create_event("FOCUS_PERIOD_STARTED")
