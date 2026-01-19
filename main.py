6e252be05f5bb49cfdba22f46586b48074a2b809
dragonbough <fiveteen81@gmail.com>
2026-01-19 10:18:40 +0000

    now exclusively launches tomo ui for testing

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from ui import todo_ui
+from ui import (tomos_ui,)
+                # todo_ui)
 
 from PyQt6.QtWidgets import QApplication
 import sys
@@ -6,11 +7,14 @@ import sys
 # sys.argv allows arguments to be passed into the QApplication from the command line
 app = QApplication(sys.argv)
 
-todo_view = todo_ui.TodoView.get_user_todo_view()
-todo_view.show()
+# todo_view = todo_ui.TodoView.get_user_todo_view()
+# todo_view.show()
 
 # when the overall application is closed carry out the quit procedure for the todoview
-app.lastWindowClosed.connect(todo_view.quit_proc)
+# app.lastWindowClosed.connect(todo_view.quit_proc)
+
+tomos_view = tomos_ui.TomoViewManager.get_tomos_view()
+tomos_view.show()
 
 # starts app event loop
 app.exec()
